39bab28ab3e20ceb5bb720734c4ed75aec71d258
Aravina03 <aravinach@gmail.com>
2021-07-19 13:07:16 +0530

    creating files done

--- index.js
+++ index.js
@@ -17,4 +17,3 @@ function countWords(paragraph, word1, word2 ) {
 }
 
 countWords('Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim Lorem quis nostrud exercitation ullamco laboris nisi ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est Lorem', 'Lorem', 'ipsum')
-
